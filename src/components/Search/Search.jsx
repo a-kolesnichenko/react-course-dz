ab9167683599c335cc59419fa73ee33da633f33b
andr-kole <andrey.kolesnichenko0@gmail.com>
2026-08-14 17:47:00 +0300

    update Search component to accept value and onChange props for controlled input

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,11 +1,11 @@
 import './Search.css';
 
-function Search({placeholder}) {
+function Search({placeholder, value, onChange}) {
 
 	return (
         <div className='search-container'>   
           <img className="search-icon" src='/search_icon.svg' alt='Поиск'/>
-		  <input className='search-custom' placeholder={placeholder} />
+		  <input className='search-custom' placeholder={placeholder} value={value} onChange={onChange} />
         </div>
 	);
 }
