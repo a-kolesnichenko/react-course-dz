b01787820da2db26c8634197a344f1077406e102
andr-kole <andrey.kolesnichenko0@gmail.com>
2026-08-18 16:43:36 +0300

    feat: implement UserContext for profile management and refactor App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { UserContext } from './contexts/User/UserContext.jsx';
 import moviesList from './MoviesList.js';
 import Header from './components/Header/Header.jsx';
 import MovieCard from './components/MovieCard/MovieCard.jsx';
@@ -8,13 +9,14 @@ import Button from './components/Button/Button.jsx';
 import Paragraph from './components/Paragraph/Paragraph.jsx';
 import Menu from './components/Menu/Menu.jsx';
 import LoginForm from './components/LoginForm/LoginForm.jsx';
-import { useLocalStorage } from './hooks/use-localstorage.hook.js';
+
 
 function App() {
    
   const [searchQuery, setSearchQuery] = useState('');
-  const [profile, setProfile] = useLocalStorage('profile', {name: null, isLogined: false});
-  const data = moviesList;
+  const { profile, setProfile } = useContext(UserContext);
+
+  const data = moviesList;  
   
   function handleLogin(name) {
 	console.log('Пользователь вошел в профиль');
@@ -27,28 +29,28 @@ function App() {
 	setProfile(nullProfile);
   }
   
-  return (
-    <div className={styles['app']}>
-     <Menu profile={profile} handleLogout={handleLogout}/>
-     <Header>Поиск</Header>
-     <Paragraph>Введите название фильма, сериала или мультфильма для поиска и добавления в избранное.</Paragraph>
-     <div className={styles['search-button-container']}>
-      <Search placeholder="Введите название" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
-      <Button onClick={() => console.log('Искать')}>Искать</Button>      
-     </div>  
-     <div className={styles['movie-cards-container']}>
-      {data.map(movie => (
-        <MovieCard 
-          key={movie.title}
-          posterSrc={movie.posterSrc}
-          title={movie.title}
-          rating={movie.rating}
-        />
-      ))}  
-     </div>   
-	 <Header>Вход</Header>
-	 <LoginForm onLogin={handleLogin} profile={profile}/>
-    </div>
+  return (	
+	<div className={styles['app']}>
+		<Menu profile={profile} handleLogout={handleLogout}/>
+		<Header>Поиск</Header>
+		<Paragraph>Введите название фильма, сериала или мультфильма для поиска и добавления в избранное.</Paragraph>
+		<div className={styles['search-button-container']}>
+		<Search placeholder="Введите название" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+		<Button onClick={() => console.log('Искать')}>Искать</Button>      
+		</div>  
+		<div className={styles['movie-cards-container']}>
+			{data.map(movie => (
+				<MovieCard 
+				key={movie.title}
+				posterSrc={movie.posterSrc}
+				title={movie.title}
+				rating={movie.rating}
+				/>
+			))}  
+		</div>   
+		<Header>Вход</Header>
+		<LoginForm onLogin={handleLogin} profile={profile}/>
+	</div>	
   );
 }
 
